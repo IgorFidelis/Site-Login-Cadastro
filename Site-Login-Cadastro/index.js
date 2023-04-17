d36fff50790fe1aa61f89535ba4b06f661d0a3e8
Igor Fidelis de Oliveira <fidelisigor50@gmail.com>
2023-04-17 18:10:12 -0300

    ajuste de posição
    ajustando posição

--- Site-Login-Cadastro/index.js
+++ Site-Login-Cadastro/index.js
@@ -2,12 +2,13 @@ const nome = document.querySelector('.nome');
 const senha = document.querySelector('.senha');
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', entrar);
 
-const entrar = ()=>{
+const entrar = ()=> {
     if (nome.value.length == 0 || senha.value.length == 0){
         window.alert('[ERRO] Preencha os dados!')
     } else {
         window.alert('Tudo certo!')
     }
 }
+
+btn.addEventListener('click', entrar);
