df66048f548dab0f0cf353d74052f29d6aae5c3c
Igor Fidelis de Oliveira <fidelisigor50@gmail.com>
2023-04-17 20:27:19 -0300

    Validador
    Juste para validar os inputs

--- Site-Login-Cadastro/index.js
+++ Site-Login-Cadastro/index.js
@@ -3,12 +3,28 @@ const senha = document.querySelector('.senha');
 const btn = document.querySelector('.btn');
 
 
+
 const entrar = ()=> {
-    if (nome.value.length == 0 || senha.value.length == 0){
-        window.alert('[ERRO] Preencha os dados!')
-    } else {
-        window.alert('Tudo certo!')
+
+    if(!nome.value){
+        let span = document.querySelector('.nome + span')
+        span.style.display = 'inline';
+        setInterval(() => {
+            span.style.display = 'none';
+        }, 5000);
+        return
     }
+
+    if(!senha.value){
+        let span = document.querySelector('.senha + span')
+        span.style.display = 'inline';
+        setInterval(() => {
+            span.style.display = 'none';
+        }, 5000);
+        return
+    }
+
+  
 }
 
 btn.addEventListener('click', entrar);
