21e1c13acc151cb1004c6e13faee132fa919ed9c
Igor Fidelis de Oliveira <fidelisigor50@gmail.com>
2023-04-18 15:07:38 -0300

    alterando JS
    tentando validar os inputs

--- Site-Login-Cadastro/index.js
+++ Site-Login-Cadastro/index.js
@@ -1,10 +1,19 @@
 const nome = document.querySelector('.nome');
 const senha = document.querySelector('.senha');
 const btn = document.querySelector('.btn');
+const inputsIS = document.querySelectorAll('input');
+
+const validar = (a)=>{
+    console.log(a);
+}
 
 
 
 const entrar = (e)=> {
+
+    inputsIS.forEach(input => {
+        validar(input);
+    });
     console.log(e);
     if(!nome.value){
         let span = document.querySelector('.nome + span')
