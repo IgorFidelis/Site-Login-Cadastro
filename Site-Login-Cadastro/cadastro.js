f694627b8243bbceaa7ff7109739fa0efa6d5d96
Igor Fidelis de Oliveira <fidelisigor50@gmail.com>
2023-04-20 11:01:21 -0300

    melhorado o projeto
    melhorando a forma de validar os inputs

--- Site-Login-Cadastro/cadastro.js
+++ Site-Login-Cadastro/cadastro.js
@@ -1,51 +1,26 @@
 const nome = document.getElementById('nome')
 const senha = document.getElementById('senha')
 const email = document.getElementById('email')
-const sexo = document.getElementsByName('genero')
 const termos = document.getElementById('aceito')  
 const btn = document.querySelector('.btn');
 const dadosIs = document.querySelectorAll('.dados');
-const sexoIs = document.querySelectorAll('.sexo');
 const termoIs = document.querySelector('.termo');
 
 
-// console.log(sexoIs);
-// console.log(termoIs);
-
-
-
 const validar = (dados)=>{
-    if(dados.className == 'sexo'){
-        if(!dados.checked ){
-            
-            return
-        }
-        return
+    console.log(dados);
+    if(dados.target.className === 'btn'){
+        console.log('btn');
+        return true
     }
 
-    if(dados.target.className == 'dados'){
-        if(!dados.target.value){
-            alert(`[ERRO] falta dados no: ${dados.target.name}`)
-            
-        }
-        console.log('sim');
-        return
-    };
-
-    console.log(dados.className);
-    // if(dados.target === 'sexo'){
-    //     console.log(dados);
-    // }
-    // console.log('outro dados');
-    // console.log('continuou');
+    if(!dados.target.value){
+        alert(`[ERRO] falta dados no: ${dados.target.name}`)
+    }
+    return
+    
 }
 
-sexoIs.forEach(sexos =>{
-    if(!sexos.checked){
-        alert(`[ERRO] confirmar o sexo.`)
-        return
-    }
-})
 
 
 dadosIs.forEach(dados =>{
@@ -54,39 +29,38 @@ dadosIs.forEach(dados =>{
 
 btn.addEventListener("click", criar)
 
-function criar(){
-    let valido1 = false
-    let valido2 = false
-    let valido3 = false
-    let genero = ''
-    let aceita = false
-    if(nome.value.length == 0 || senha.value.length == 0 || email.value.length == 0){
-        window.alert('[ERRO] Falta dados!');
-    } else {
-        valido1 = true
-    }
+function criar(e){
+    console.log(e);
 
-    if(sexo[0].checked){
-        genero = 'masculino'
-         valido2 = true
-    } else if(sexo[1].checked){
-        genero = 'feminino'
-        valido2 = true
-    } else if(sexo[2].checked){
-        genero = 'outro'
-        valido2 = true
-    } else {
-        window.alert('[ERRO] Informe seu Genero!')
+   if(!validar(e)){
+       return
     }
     
-    if(termos.checked){
-        aceita = true
-        valido3 = true
-    } else {
-        window.alert('[ERRO] Termos não aceitos!')
+    console.log('chegou');
+
+
+    let sexo1 = document.querySelector('.sexo:checked');
+    if(!sexo1){
+        let spanGeneroIS = document.querySelector(`.radio  span`);
+        console.log(spanGeneroIS);
+        spanGeneroIS.style.display = 'block'
+        setInterval(() => {
+            spanGeneroIS.style.display = "none"
+        }, 3000);
+        return
     }
 
-    if(valido1 === true && valido2 === true && valido3 === true)  {
-        alert('Conta Criada!')
+    if(!termoIs.checked){
+        let spanTemosIS = document.querySelector(`.termos  span`);
+        console.log(spanTemosIS);
+        spanTemosIS.style.display = 'block'
+        setInterval(() => {
+            spanTemosIS.style.display = "none"
+        }, 3000);
+        return
     }
+
+
+        alert('Conta Criada!')
+    
 }
