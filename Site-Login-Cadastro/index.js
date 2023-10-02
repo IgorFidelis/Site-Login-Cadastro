a0a07ec7a6bae66a29e12ca985a6e7846b97160d
Igor Fidelis de Oliveira <fidelisigor50@gmail.com>
2023-10-02 10:19:12 -0300

    criando function
    criando function para melhor o desempenho

--- Site-Login-Cadastro/index.js
+++ Site-Login-Cadastro/index.js
@@ -3,14 +3,20 @@ const senha = document.querySelector('.senha');
 const btn = document.querySelector('.btn');
 const inputsIS = document.querySelectorAll('input');
 
+
+const stopTima = (time)=>{
+    clearTimeout(time)
+}
+
 const validar = (a)=>{
     if(!a.value){
         let span = document.querySelector(`.${a.className} + span`)
         span.style.display = 'inline';
         let time = setTimeout(() => {
             span.style.display = 'none';
+            stopTima(time);
         }, 2000);
-
+        
         
     }
 }
